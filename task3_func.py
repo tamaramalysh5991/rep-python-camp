41632a117f8a5745ac5c291e1986b5b980ee5122
Tamara Malysheva <tamara.malysheva@saritasa.com>
2017-07-11 18:54:36 +0700

    Done task3

--- task3_func.py
+++ task3_func.py
@@ -1,31 +1,28 @@
-"""Implement state-aware integers() generator.
-
-        def integers():
-        i = 1
-        while True:
-            yield i
-            i = i + 1
-
-    desired outcome:
-
-            i1 = integers()
-            next(i1)
-            1
-            next(i1)
-            2
-            next(i1)
-            3
-            i2 = integers()
-            next(i2)
-            4
-"""
-
 def integers():
-    if not hasattr(integers, 'identifier'):
+    """Implement state-aware integers() generator.
+    An infinite number generator, from 1 to enum.
+        Args:
+            label (str):  The label indicates whether the function is
+            called the first time or not.
+        Yields:
+            Generates an infinite sequence in increments of 1
+        Example:
+                    i1 = integers()
+                    next(i1)
+                    1
+                    next(i1)
+                    2
+                    next(i1)
+                    3
+                    i2 = integers()
+                    next(i2)
+                    4
+    """
+    if not hasattr(integers, 'label'):
         def count():
             counter = 1
             while True:
                 yield counter
                 counter += 1
-        integers.identifier = count()
-    return integers.identifier
+        integers.label = count()
+    return integers.label
