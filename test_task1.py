5ce9ab92990a3e9aa196d2c633c6f0b0188b259c
Tamara Malysheva <tamara.malysheva@saritasa.com>
2017-07-12 11:20:37 +0700

    test_task1

---
+++ test_task1.py
@@ -0,0 +1,26 @@
+from task1_func import reverse_iter
+import unittest
+
+
+class Test_task1(unittest.TestCase):
+
+    def setUp(self):
+        self.reverse_exp = reverse_iter
+
+    def test_exception(self):
+        with self.assertRaises(ValueError):
+            next(self.reverse_exp([]))
+        with self.assertRaises(ValueError):
+            next(self.reverse_exp(''))
+        with self.assertRaises(AttributeError):
+            next(self.reverse_exp(2))
+
+    def test_0(self):
+        s = reverse_iter([1, 2, 3])
+        self.assertEqual(next(s), 3)
+        self.assertEqual(next(s), 2)
+        self.assertEqual(next(s), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
