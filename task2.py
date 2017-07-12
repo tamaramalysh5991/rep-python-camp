a1549e8ef3d367333cf26342469271a5dd993396
Tamara Malysheva <tamara.malysheva@saritasa.com>
2017-07-12 18:54:39 +0700

    create test for 0,1,3,2 update task2

--- task2.py
+++ task2.py
@@ -9,11 +9,10 @@ def search_in_files(pattern, files):
     Args:
         pattern(string) - Word used for search.
         files (file)- List of received files.
-    Return:
-        list: rows with an occurrence pattern.
     Yields:
         int: line_count - count of occurrence.
         int: all_processed - total processed rows
+        list: find_rows - rows with pattern
     """
     encounters = 0
     all_processed = 0
@@ -29,16 +28,14 @@ def search_in_files(pattern, files):
 
 
 def search_lines(pattern, lines):
-    """Funcrion return search results from standard input.
+    """Funcrion return search results from files input.
     Args:
         pattern(string) - Word used for search.
-        lines(list)- Rows.
-    Return:
-        list: rows with an occurrence pattern.
+        lines (iter)- List of received lines.
     Yields:
         int: line_count - count of occurrence.
         int: all_processed - total processed rows
-    """
+        list: find_rows - rows with pattern"""
     lines = iter(lines)
     encounters = 0
     all_processed = 0
@@ -56,39 +53,39 @@ def search_lines(pattern, lines):
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
+
 @click.command(context_settings=CONTEXT_SETTINGS)
 def cli():
-    ''' task2_click.py is programm for find occurrence PATTERN in files\n
-        ./task2_click.py  PATTERN file1 file2 or < test.py (stdin)\n
+    ''' task_2.py is programm for find occurrence PATTERN in files\n
+        ./task_2.py  PATTERN file1 file2 or < test.py (stdin)\n
         \b
-        cat file1 | ./task2_click.py PATTERN\n
-        find . -iname '*.py' | ./task2_click.py  PATTERN
+        cat file1 | ./task_2.py PATTERN\n
+        find . -iname '*.py' | ./task_2.py  PATTERN
     '''
     pass
+
 
 @click.command()
 @click.argument('pattern', type=str)
 @click.argument('files', nargs=-1, type=click.File('r'))
 def grep_file(pattern, files):
-    """
-    """
+    """Search in file (./task2_click.py  PATTERN file1 file2)"""
     result = 'Find lines: \n%s\n %d occurrences in %d rows.\n'
     encounters, all_processed, find_rows = next((search_in_files(pattern, files)))
     click.secho(result % (find_rows, encounters, all_processed), fg='blue')
 
+
 @click.command()
 @click.argument('pattern', nargs=1, type=str)
 def grep_input(pattern):
-    """
-    """
+    """Search in lines (/task_2.py  PATTERN < test.py (stdin)"""
     result = 'Find lines: \n%s\n %d occurrences in %d rows.\n'
     input_stream = click.get_text_stream('stdin')
     encounters, all_processed, find_rows = next(search_lines(pattern, input_stream))
     click.secho(result % (find_rows, encounters, all_processed), fg='blue')
 
 
-
-if __name__ == '__main__': # pragma: no cover
+if __name__ == '__main__':  # pragma: no cover
     if not stdin.isatty():
         grep_input()
     else:
