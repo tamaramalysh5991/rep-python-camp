b9c3eababc24bab2d7ad72ed63bc31227df42a18
Tamara Malysheva <tamara.malysheva@saritasa.com>
2017-07-18 18:49:31 +0700

    update Family class

--- Day6/Human.py
+++ Day6/Human.py
@@ -1,85 +1,66 @@
 import abc
-from datetime import  datetime, date, time
+# from datetime import  datetime, date, time
 import itertools
-import gc
-
 import weakref
+import uuid
+
 
 class Person(abc.ABC):
+    '''Base class of Family tree
+    Initializes a Person
+
+    '''
     ID = itertools.count()
     isinstances = []
 
-
-    def __init__(self, first_name, last_name, birth, gender, height, weight, married = False, spouse = None, parent = None, level = 0):
+    def __init__(self, first_name, last_name, birth, gender):
         self.__class__.isinstances.append(weakref.proxy(self))
         self.first_name = first_name
         self.id = next(self.__class__.ID)
         self.last_name = last_name
         self.birth = birth
         self.gender = gender
-        self.height = int(height)
-        self.weight = int(weight)
-        self.parent = parent
-        self.level = level
-        self.children = []
-        #self.married - married
+        # self.parent = parent
+        # self.married - married
         #   self.created = created or datetime.now()=
 
+    def sex(self):
+        pass
 
-    #def age(self, birth):
-        #age = datetime.now() - datetime(birth)
+    def add_mother(self):
+        pass
 
-    def dic(self):
+    def add_father(self):
+        pass
+
+    def __dict__(self):
         attr = vars(self)
-        #return attr
         for key in attr:
-            print(key, ':', attr[key],'\n')
+            print(key, ':', attr[key], '\n')
 
     def __str__(self):
-        print( '[Person: %s, %s]', self.first_name, self.last_name)
-
-
-
-
-
-
+        ''''''
+        print('[Person: %s, %s]', self.first_name, self.last_name)
 
 
 class Woman(Person):
-    def __init__(self, first_name, last_name, birth, gender, height, weight, married = False, spouse = None,  husband = None, parent = None, level = 0, maiden_name=None  ):
-        super().__init__(first_name, last_name, birth, gender, height, weight, married = False, spouse = None,  parent = None, level=0)
-
+    def __init__(self, first_name, last_name, birth, gender, maiden_name=None):
+        super().__init__(first_name, last_name, birth, gender)
 
 
 class Man(Person):
-    def __init__(self, first_name, last_name, birth, gender, height, weight, married = False,  spouse = None, wife = None, parent = None, level = 0):
-        super().__init__( first_name, last_name, birth, gender, height, weight, married = False,  spouse = None, parent=None, level=0)
+    def __init__(self, first_name, last_name, birth, gender):
+        super().__init__(first_name, last_name, birth, gender)
 
     def propose(self):
         pass
 
 
-
-def marriage(Woman, Man):
-    Woman.married = True
-    Man.married = True
-    Woman.spouse = Man.first_name
-    Woman.husband = Man.first_name
-    Woman.maiden_name = Woman.last_name
-    Woman.last_name = Man.last_name
-    Man.spouse = Woman.first_name
-    Man.wife = Woman.first_name
-
-def print_all():
-    for instance in Person.isinstances:
-        print(instance.first_name)
-        #print(instance.__dict__)
-
-'''def Parent(Person.y, Person.q):
-    pass'''
-
-
 class Family:
+    '''Class of Family'''
+
+    # def __init__(self):
+    family_id = uuid.uuid4()
 
     def mother(self):
         pass
@@ -93,19 +74,21 @@ class Family:
     def brother(self):
         pass
 
+    def marriage(Woman, Man):
+        '''marriage and create new Family'''
+        Family()
+        Woman.married = True
+        Man.married = True
+        Woman.spouse = Man.first_name
+        Woman.husband = Man.first_name
+        Woman.maiden_name = Woman.last_name
+        Woman.last_name = Man.last_name
+        Man.spouse = Woman.first_name
+        Man.wife = Woman.first_name
+        Woman.id = Family.family_id
+        Man.id = Family.family_id
 
 
-#Toma = Person('Green', 1995, 'f', 165, 55)
-
-'''def ancestors(genealogy, person):
-	if person in genealogy:
-		parents = genealogy[person]
-		result = parents
-		for parent in parents:
-			result = result + ancestors(genealogy,parent)
-		return result
-	return []'''
-
-# Tamara = Woman('Toma', 'Malysheva', 1995, 'f', 164, 55)
-# Denis = Man('Denis', 'Tverd', 1992, 'm', 179, 75)
+Tamara = Woman('Toma', 'Malysheva', 1995, 'f')
+Denis = Man('Denis', 'Tverd', 1992, 'm')
 # Andrey  = Man('Andrey', 'Shilov', 1991, 'm', 179, 75)
