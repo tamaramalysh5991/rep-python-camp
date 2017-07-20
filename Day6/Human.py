2bb512417027a7352bbee879ffa8ce85399b0415
Tamara Malysheva <tamara.malysheva@saritasa.com>
2017-07-20 19:02:13 +0700

    add create baby, sister, brother, add propose

--- Day6/Human.py
+++ Day6/Human.py
@@ -4,6 +4,7 @@ import itertools
 import weakref
 import uuid
 import random
+import names
 
 
 class Person(abc.ABC):
@@ -11,7 +12,7 @@ class Person(abc.ABC):
     Initializes a Person.
 
     Atributes:
-        first_name (str) =  Human readable string describing first name
+        first_name (str) =  Human readable string describing first nameDenis.Ta
         last_name (str) =  Human readable string describing last name
         id (uuid) = generete uniq id
         b
@@ -27,29 +28,12 @@ class Person(abc.ABC):
         self.last_name = last_name
         self.birth = birth
         self.gender = gender
-        self.fertility = random.triangular()
+        self.fertility = random.random()
         self.spouse = None
+        self.family = Family()
         # self.parent = parent
         # self.married - married
         #   self.created = created or datetime.now()=
-
-    def sex(self, person):
-        if self.spouse == person and person.spouse == self:
-            self.fertility = random.triangular()
-            person.fertility = random.triangular()
-            total_fertility = self.fertility + person.fertility - (self.fertility * person.fertility)
-            if total_fertility > 0.5:
-                print('baby!')
-            else:
-                print('Ops!')
-        else:
-            print('It not a spouse')
-
-    def add_mother(self):
-        pass
-
-    def add_father(self):
-        pass
 
 
 class Family:
@@ -57,58 +41,126 @@ class Family:
 
     family_id = uuid.uuid4()
 
-    def __init__(self, father, mother):
+    def __init__(self, father=None, mother=None):
         self.mother = mother
         self.father = father
+        self.sister = []
+        self.brother = []
+        self.children = []
 
-    def sister(self):
-        pass
+    """def add_sister_brother(self):
+        for child in self.children:
+            if child.gender == 'm':
+                self.brother.append(child)
+            else:
+                self.sister.append(child)"""
 
-    def brother(self):
-        pass
+
+class PersonMixin(object):
+    """Class PersonMixin"""
+
+    # @staticmethod
+    def marriage(self, person):
+        """marriage and create new Family"""
+        if not (self.propose or person.propose):
+            raise AttributeError('They are not engaged!')
+        f = Family()
+        person.family = f
+        self.family = f
+        person.spouse = self
+        self.spouse = person
+        if isinstance(person, Woman):
+            person.maiden_name = person.last_name
+            person.last_name = self.last_name
+            person.status = 'Wife'
+            self.status = 'Husband'
+            f.mother = person
+            f.father = self
+        else:
+            self.maiden_name = self.last_name
+            self.last_name = person.last_name
+            self.status = 'Wife'
+            person.status = 'Husband'
+            f.mother = self
+            f.father = person
+
+    # @classmethod
+    def sex(self, person):
+        """New member of family"""
+
+        if not (self.spouse == person and person.spouse == self):
+            AttributeError('It not a spouse')
+        total_fertility = self.fertility + person.fertility - (self.fertility * person.fertility)
+        if total_fertility > 0.5:
+            print('baby!')
+            gender = ['f', 'm']
+            g = random.choice(gender)
+            if g == 'f':
+                name = names.get_first_name(gender='female')
+                baby = Woman(name, person.last_name, 2017, g)
+            else:
+                name = names.get_first_name(gender='male')
+                baby = Man(name, person.last_name, 2017, g)
+            baby.family = person.family
+            person.family.children.append(baby)
+            if isinstance(person, Woman):
+                baby.family.mother = person
+                baby.family.father = self
+            else:
+                baby.family.mother = self
+                baby.family.father = person
+            if len(person.family.children) != 0:
+                # person.family.add_sister_brother()
+                if isinstance(baby, Man):
+                    person.family.brother.append(baby)
+                else:
+                    person.family.sister.append(baby)
+        else:
+                print('Ops!')
 
 
-class Woman(Person, Family):
-    def __init__(self, first_name, last_name, birth, gender, maiden_name=None, spouse=None):
+class Woman(Person, Family, PersonMixin):
+    """Class Woman(Person)"""
+
+    def __init__(self, first_name, last_name, birth, gender,
+                 status=None, propose=False, maiden_name=None, spouse=None):
         super().__init__(first_name, last_name, birth, gender)
         self.spouse = spouse
         self.maiden_name = maiden_name
         self.root_id = self.id
-
-    def marriage(self, man):
-        """marriage and create new Family"""
-        Family(man, self)
-        man.spouse = self
-        self.maiden_name = self.last_name
-        self.last_name = man.last_name
-        self.spouse = man
-        self.id = Family.family_id
-        man.id = Family.family_id
+        self.propose = propose
+        self.status = status
 
 
-class Man(Person, Family):
-    def __init__(self, first_name, last_name, birth, gender, spouse=None):
+class Man(Person, Family, PersonMixin):
+    """Class Man (Person)"""
+
+    def __init__(self, first_name, last_name, birth,
+                 gender, propose=False, status=None, spouse=None):
         super().__init__(first_name, last_name, birth, gender)
         self.spouse = spouse
         self.root_id = self.id
+        self.propose = propose
+        self.status = status
 
-    def propose(self):
-        pass
+    def proposed(self, person):
+        if isinstance(person, Man):
+            raise AttributeError('You dont"t married on man!')
 
-    # @staticmethod
-    def marriage(self, woman):
-        """marriage and create new Family"""
-        Family(self, woman)
-        woman.spouse = self
-        woman.maiden_name = woman.last_name
-        woman.last_name = self.last_name
-        self.spouse = woman
-        woman.id = Family.family_id
-        self.id = Family.family_id
+        probability_of_consent = random.random()
+        if probability_of_consent:  # > 0.5
+            print('She say Yes!')
+            person.propose = True
+            self.propose = True
+            person.status = 'Fiancee'
+            self.status = 'Fiance'
 
-Andrey  = Man('Andrey', 'Shilov', 1991, 'm')
+
+Andrey = Man('Andrey', 'Shilov', 1991, 'm')
 
 Tamara = Woman('Toma', 'Malysheva', 1995, 'f')
 Denis = Man('Denis', 'Tverd', 1992, 'm')
+Denis.proposed(Tamara)
 Denis.marriage(Tamara)
+Denis.sex(Tamara)
 
